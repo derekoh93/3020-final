2eaf3403c0be2378489dd3fea8d4ffa815f491a9
derek.oh93 <derek.oh93@gmail.com>
2017-03-08 17:18:00 -0500

    added map and markers

--- bootstrap-3.3.7-dist/js/main.js
+++ bootstrap-3.3.7-dist/js/main.js
@@ -24,11 +24,33 @@ $(document).ready(function(){
     "Streets": streetMap,
     "Outdoors": outdoorsMap,
   };
+
+  //These are the different markers on the map with popup descriptions
+  var elevated = L.marker([47.8317, -122.2080]).bindPopup('Elevated Sportz, place for fun with family'),
+      airportPark = L.marker([47.8638, -122.2379]).bindPopup('Airport Park, nice place to relax and explore'),
+      marthaPark = L.marker([47.8507304, -122.2425288]).bindPopup('Martha Lake Park, beautiful trails and lake'),
+			azul = L.marker([47.8608,-122.2209]).bindPopup('Azul Restaurant, Nice restaurant and lounge to get food'),
+			zen = L.marker([47.8776, -122.2121]).bindPopup('Sushi Zen, place for nice sushi'),
+      pelican = L.marker([47.8555, -122.2196]).bindPopup('Rusty Pelican, restaurant for breakfast and lunch'),
+      palmera = L.marker([47.8593, -122.2206]).bindPopup('La Palmera, restaurant for some nice mexican food'),
+			tablas = L.marker([47.8568,-122.2210]).bindPopup('Tablas Woodstone Taverna, enjoy nice music and good food'),
+			sawmill = L.marker([47.8582, -122.2193]).bindPopup('Saw Mill Cafe, nice homestyle cafe for families'),
+      shawn = L.marker([47.8815, -122.2302]).bindPopup('Shawn O Donnell, american grill and irish pub'),
+      brunswick = L.marker([47.8482, -122.2501]).bindPopup('Brunswick Lanes, fun bowling with family'),
+			towncenter = L.marker([47.8601,-122.2043]).bindPopup('Mill Creek Towncenter, enjoy shopping, food and entertainment')
+			
+			
+  //This is the layer group of markers that you can find on the map   
+  var visit = L.layerGroup([paradise,longmire,naradaFalls,azul,zen,pelican,palmera,tablas,sawmill,shawn,brunswick,towncenter]);
+  
+  var mapOverlay = {
+      "visit": visit
+  };
     //Default position and zoom of the map when you start the page up     
   var myMap = L.map('map-container', {
     center: [47.855, -122.218],
     zoom: 14,
-    layers: [satMap, visit]
+    layers: [satMap,visit]
   });
     
   $("a").on('click', function(event) {
